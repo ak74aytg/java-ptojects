6bb292bd7d444fe16b55b1c274f88ae2d9e769c1
Akshay <ak74ay@gmail.com>
2024-10-27 19:20:00 +0530

    commit 1

--- program.java
+++ program.java
@@ -2,5 +2,6 @@ public class program {
 
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hii there i am using jenkins test project");
     }
 }
