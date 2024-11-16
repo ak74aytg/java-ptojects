870d854d1f2e0e7e93d917128dd3152de64abb6b
Akshay <ak74ay@gmail.com>
2024-11-16 11:56:03 +0530

    jenkins testing

--- program.java
+++ program.java
@@ -3,5 +3,7 @@ public class program {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hii there i am using jenkins test project");
+        System.out.println("what is up bitches!!");
+        System.out.println("go fuck yourselfs");
     }
 }
